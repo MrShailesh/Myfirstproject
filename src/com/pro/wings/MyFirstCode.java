51520e3ed540cadd3d4e1f2707de88f7be490069
Your Name <you@example.com>
2021-10-08 11:07:41 +0530

    my 1st comment

---
+++ src/com/pro/wings/MyFirstCode.java
@@ -0,0 +1,10 @@
+package com.pro.wings;
+
+public class MyFirstCode
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hey its my 1st code");
+        System.out.println("hi");
+    }
+}
